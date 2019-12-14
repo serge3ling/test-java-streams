2884b4f5ed7f078e9c88b8446cd7bfc14cf91382
Sergio Trett <serge3ling@gmail.com>
2019-12-14 18:26:41 +0200

    Almost empty structure.

---
+++ run-main.sh
@@ -0,0 +1,1 @@
+java -cp bin/classes com.herokuapp.s3_test_streams.Main
---
+++ src/com/herokuapp/s3_test_streams/Main.java
@@ -0,0 +1,5 @@
+package com.herokuapp.s3_test_streams;
+public class Main {
+    public static void main(String[] args) {
+    }
+}
